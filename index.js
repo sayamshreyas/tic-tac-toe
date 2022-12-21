a05f0162b1a449b7efa5a6aab5435be39a635419
sayamshreyas <95953945+sayamshreyas@users.noreply.github.com>
2022-12-21 11:51:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+'use strict';
+const os = require('os');
+const path = require('path');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const homeDirectory = os.homedir();
+const {env} = process;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+exports.data = env.XDG_DATA_HOME ||
+	(homeDirectory ? path.join(homeDirectory, '.local', 'share') : undefined);
+
+exports.config = env.XDG_CONFIG_HOME ||
+	(homeDirectory ? path.join(homeDirectory, '.config') : undefined);
+
+exports.cache = env.XDG_CACHE_HOME || (homeDirectory ? path.join(homeDirectory, '.cache') : undefined);
+
+exports.runtime = env.XDG_RUNTIME_DIR || undefined;
+
+exports.dataDirs = (env.XDG_DATA_DIRS || '/usr/local/share/:/usr/share/').split(':');
+
+if (exports.data) {
+	exports.dataDirs.unshift(exports.data);
+}
+
+exports.configDirs = (env.XDG_CONFIG_DIRS || '/etc/xdg').split(':');
+
+if (exports.config) {
+	exports.configDirs.unshift(exports.config);
+}
